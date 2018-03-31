d0e2d632fabb87f10f69377d4abbed5c4dcd10cf
LOVE\Konrad <1konrad15@gmail.com>
2018-03-31 02:44:18 +0200

    Added retarded mob to stage

---
+++ core/src/com/mygdx/game/entities/Mob.java
@@ -0,0 +1,48 @@
+package com.mygdx.game.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.mygdx.game.MyGdxGame;
+
+public class Mob extends Image
+{
+	private final static int WIDHT = 80;
+	private final static int HEIGHT = 80;
+	
+	private final static int STARTING_X = -100; //TODO this should be current 
+	private final static int STARTING_Y = 500;  // stage base (X,Y)
+	
+	private MyGdxGame game;
+	
+	public Mob(MyGdxGame game)
+	{
+		super(new Texture("badlogic.jpg"));		
+		this.game = game;
+		
+		this.setOrigin(WIDHT / 2, HEIGHT / 2);
+		this.setSize(WIDHT, HEIGHT);
+		
+		this.setPosition(STARTING_X, STARTING_Y);
+		
+		//on click show mob info
+	}
+	
+	public void followPath()
+	{
+		Action a = Actions.parallel(Actions.moveBy(300, 0, 5));
+		
+		Action b = Actions.parallel(Actions.moveBy(0, -200, 4));
+		
+		Action c = Actions.run(new Runnable() {
+		
+			public void run() {
+				Mob.this.remove();
+			}
+		});
+		
+		this.addAction(Actions.sequence(a, b, c));
+	}
+	
+}

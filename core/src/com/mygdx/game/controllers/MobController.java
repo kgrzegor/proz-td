d674470b890e08bcc4fccfd7924e7ef8a5d78775
LOVE\Konrad <1konrad15@gmail.com>
2018-05-24 22:47:42 +0200

    Factory Added in MobControllerClass

--- core/src/com/mygdx/game/controllers/MobController.java
+++ core/src/com/mygdx/game/controllers/MobController.java
@@ -6,8 +6,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 import com.mygdx.game.entities.PowerUp;
-import com.mygdx.game.entities.Mob;
-import com.mygdx.game.entities.MobInterface;
+import com.mygdx.game.entities.enemies.EnemyFactory;
+import com.mygdx.game.entities.enemies.Mob;
+import com.mygdx.game.entities.enemies.MobType;
 import com.mygdx.game.services.GoldService;
 import com.mygdx.game.services.PlayerLivesService;
 import com.mygdx.game.services.PointsService;
@@ -21,17 +22,19 @@ public class MobController implements PowerUp
 	private ArrayList<Mob> mobsList;
 	private GoldService goldService;
 	private PointsService pointsService;
+	private EnemyFactory enemyFactory;
 
 	public MobController(Stage stage, PlayerLivesService playerLivesService, GoldService goldService,
 			PointsService pointsService)
 	{
-		spawnTime = 3f;
-		spawnCount = 15;
+		this.spawnTime = 3f;
+		this.spawnCount = 15;
 		this.stage = stage;
 		this.playerLivesService = playerLivesService;
 		this.goldService = goldService;
 		this.pointsService = pointsService;
-		mobsList = new ArrayList<Mob>();
+		this.mobsList = new ArrayList<Mob>();
+		this.enemyFactory = new EnemyFactory(this);
 	}
 
 	public void startWave(int id)
@@ -47,31 +50,35 @@ public class MobController implements PowerUp
 
 	private void addMobToStage()
 	{
-		Mob newMob = new Mob(new MobInterface()
-		{
-			@Override
-			public void die(Mob mob)
-			{
-				goldService.addGold(mob.getGold()); // magic numbers
-				pointsService.addPoints(mob.getPoints());
-			}
-
-			@Override
-			public void makeDamage()
-			{
-				playerLivesService.makeDamage();
-			}
-
-			@Override
-			public void removeFromStage(Mob mob)
-			{
-				mobsList.remove(mob);
-				mob.remove();
-			}
-		});
+		Mob newMob = enemyFactory.createMob(MobType.Yeti);
 		stage.addActor(newMob);
-		newMob.followPath();
 		mobsList.add(newMob);
+	}
+
+	public void damagePlayer(Mob mob)
+	{
+		playerLivesService.makeDamage();
+		removeFromStage(mob);
+	}
+
+	public void mobDied(Mob mob)
+	{
+		goldService.addGold(mob.getGold());
+		pointsService.addPoints(mob.getPoints());
+		removeFromStage(mob);
+
+	}
+
+	private void removeFromStage(Mob mob)
+	{
+		mobsList.remove(mob);
+		mob.remove();
+	}
+
+	public void powerUpEffect(float time)
+	{
+		for (Mob m : mobsList)
+			m.freeze(time / 10);
 	}
 
 	public ArrayList<Mob> getMobsList()
@@ -79,10 +86,4 @@ public class MobController implements PowerUp
 		return mobsList;
 	}
 
-	public void popoutEffect(float time)
-	{
-		time /= 10;
-		for (Mob m : mobsList)
-			m.freeze(time);
-	}
 }

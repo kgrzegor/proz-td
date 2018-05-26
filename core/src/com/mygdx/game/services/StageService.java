4ff66f627745e64fd62aab77bc18a0db9eab51da
LOVE\Konrad <1konrad15@gmail.com>
2018-05-26 15:41:53 +0200

    formatting fixes

--- core/src/com/mygdx/game/services/StageService.java
+++ core/src/com/mygdx/game/services/StageService.java
@@ -1,17 +1,17 @@
 package com.mygdx.game.services;
 
-
 public class StageService
 {
 	private int currentStage;
 	private int lastStage;
 	private final float spawnTime;
-	private final int[] spawnCount = {3, 2};
+	private final int[] spawnCount = { 4, 1 };
+
 	public StageService()
 	{
 		this.currentStage = 0;
 		this.lastStage = 2;
-		this.spawnTime = 3f;
+		this.spawnTime = 1f;
 	}
 
 	public void nextStage()
@@ -19,19 +19,22 @@ public class StageService
 		if (currentStage < lastStage)
 			++currentStage;
 	}
+
 	public int getCurrentStage()
 	{
 		return currentStage;
 	}
+
 	public int getLastStage()
 	{
 		return lastStage;
 	}
+
 	public boolean hasNextStage()
 	{
-		return !(currentStage == lastStage);
+	  return !(currentStage == lastStage);
 	}
-	
+
 	public float getSpawnTime()
 	{
 		return spawnTime;
@@ -41,6 +44,7 @@ public class StageService
 	{
 		return spawnCount[currentStage - 1];
 	}
+
 	public int getSpawnCountLastStage()
 	{
 		return spawnCount[spawnCount.length - 1];

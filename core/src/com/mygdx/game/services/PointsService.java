076416ac67bdb0318db3aac2ddf5161caac36fcd
LOVE\Konrad <1konrad15@gmail.com>
2018-05-26 15:42:44 +0200

    HighScore are now saved

--- core/src/com/mygdx/game/services/PointsService.java
+++ core/src/com/mygdx/game/services/PointsService.java
@@ -1,12 +1,28 @@
 package com.mygdx.game.services;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class PointsService
 {
+	public final static String GAME_PREFS = "com.mygdx.game.prefs";
+	public final static String GAME_HIGHSCORE = "com.mygdx.game.prefs.highscore";
+	
+	private Preferences prefs;
 	private int points;
+	private int highscore;
 
 	public PointsService()
 	{
 		points = 0;
+		
+		prefs =  Gdx.app.getPreferences(GAME_PREFS);
+		loadHighscore();
+	}
+
+	private void loadHighscore()
+	{
+		highscore = prefs.getInteger(GAME_HIGHSCORE);
 	}
 
 	public int getPoints()
@@ -18,4 +34,19 @@ public class PointsService
 	{
 		this.points += points;
 	}
+
+	public int getHighscore()
+	{
+		if (points >= highscore)
+		{
+			highscore = points;
+			prefs.putInteger(GAME_HIGHSCORE, highscore);
+		}
+		
+		return highscore;
+	}
+
+	
+	
+	
 }

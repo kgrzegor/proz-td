9321a57a9e984670d3f6cfd56bee29064cef0a10
LOVE\Konrad <1konrad15@gmail.com>
2018-05-26 17:13:59 +0200

    MainMenu button in endgame screen

--- core/src/com/mygdx/game/screens/EndGameScreen.java
+++ core/src/com/mygdx/game/screens/EndGameScreen.java
@@ -3,20 +3,50 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.screens.ui.GameButton;
 import com.mygdx.game.screens.ui.GameLabel;
+import com.mygdx.game.screens.ui.IClickCallback;
+import com.mygdx.game.services.PointsService;
 
 public class EndGameScreen extends AbstractScreen
 {
 
 	private Image endImg;
+	private PointsService pointsService;
 
 	public EndGameScreen(MyGdxGame game, String text)
 	{
 		super(game);
+		pointsService = game.getPointsService();
+
+		initLabels(text);
+		initMainMenuButton();
+	}
+
+	private void initMainMenuButton()
+	{
+		GameButton mainMenuButton = new GameButton.Builder(new IClickCallback()
+		{
+			
+			@Override
+			public void onClick()
+			{
+				game.create();
+				
+			}
+		}).debug(true).position(500, 200).width(50).height(50).build();
 		
+		stage.addActor(mainMenuButton);
+		
+	}
+
+	private void initLabels(String text)
+	{
 		GameLabel gameoverLabel = new GameLabel(stage, 200, 500, "fontbig.fnt");
 		gameoverLabel.setText(text);
 
+		GameLabel scoreLabel = new GameLabel(stage, 200, 400);
+		scoreLabel.setText("Your Points: " + pointsService.getPoints() + "\nHighscore: " + pointsService.getHighscore());
 	}
 
 	@Override

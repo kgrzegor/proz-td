6bd72fe83dbbac4e5a4a57b17c6ee5f62ff36511
LOVE\Konrad <1konrad15@gmail.com>
2018-05-28 23:07:02 +0200

    UpgradeException added

---
+++ core/src/Exceptions/UpgradeException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+@SuppressWarnings("serial")
+public class UpgradeException extends Exception
+{
+	public UpgradeException(String text)
+	{
+		super("Tower" + text + "at max level!");
+	}
+}

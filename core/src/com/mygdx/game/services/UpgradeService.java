27fd80d2a04e3e8eea358181b65bce11751734e5
LOVE\Konrad <1konrad15@gmail.com>
2018-05-29 10:46:41 +0200

    formatting fixes

--- core/src/com/mygdx/game/services/UpgradeService.java
+++ core/src/com/mygdx/game/services/UpgradeService.java
@@ -4,9 +4,9 @@ import com.mygdx.game.exceptions.UpgradeException;
 
 public class UpgradeService
 {
-	private static final int[] rangeCost = { 100, 150, 200, 300, 500, 1000, 2000, 0};
-	private static final int[] damageCost = { 200, 300, 400, 500, 1000, 2000, 3000, 5000, 0};
-	private static final int[] fireRateCooldownCost = { 200, 500, 1000, 1500, 2000, 5000, 9000, 0};
+	private static final int[] rangeCost = { 100, 150, 200, 300, 500, 1000, 2000, 0 };
+	private static final int[] damageCost = { 200, 300, 400, 500, 1000, 2000, 3000, 5000, 0 };
+	private static final int[] fireRateCooldownCost = { 200, 500, 1000, 1500, 2000, 5000, 9000, 0 };
 
 	private int rangeLvl, damageLvl, fireRateCooldownLvl;
 

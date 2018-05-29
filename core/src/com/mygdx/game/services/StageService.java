066bff039ecb66bc1abbd99c5d0783350591ef3d
LOVE\Konrad <1konrad15@gmail.com>
2018-05-29 10:21:13 +0200

    formatting fixes

--- core/src/com/mygdx/game/services/StageService.java
+++ core/src/com/mygdx/game/services/StageService.java
@@ -9,9 +9,10 @@ public class StageService
 {
 	private int currentStage;
 	private int lastStage;
-	private final float[] spawnTime = {3,3,1,5,3,3,5};
-	private final int[] spawnCount = { 10, 10, 30, 15,5,10,1 };
-	private final MobType[] waveType = { MobType.Demon, MobType.Yeti, MobType.Snail, MobType.RedDemon, MobType.BiggerYeti, MobType.Mummy, MobType.Boss};
+	private final float[] spawnTime = { 3.5f, 5, 1, 4, 7, 3, 5 };
+	private final int[] spawnCount = { 15, 10, 50, 15, 8, 20, 1 };
+	private final MobType[] waveType = { MobType.Demon, MobType.Yeti, MobType.Snail, MobType.RedDemon,
+			MobType.BiggerYeti, MobType.Mummy, MobType.Boss };
 
 	public StageService()
 	{
